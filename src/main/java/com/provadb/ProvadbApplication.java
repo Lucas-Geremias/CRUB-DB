da05a4f9628f5ad5bedd06e64133a188f072e844
Lucas-Geremias <geremiaslucas2012@gmail.com>
2020-10-28 12:08:19 -0300

    Começando

---
+++ src/main/java/com/provadb/ProvadbApplication.java
@@ -0,0 +1,13 @@
+package com.provadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvadbApplication.class, args);
+	}
+
+}
